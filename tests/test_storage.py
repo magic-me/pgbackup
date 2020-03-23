abfd221811980a60164b594f54997a85063538ec
Anton Illarionov <dremlin@gmail.com>
2020-03-23 15:36:04 +0300

    implemented local and s3 storage

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,17 +1,29 @@
-import tempfile
+import tempfile, pytest
 
 from pgbackup import storage
 
-def test_storing_file_locally():
+@pytest.fixture()
+def infile():
+    f = tempfile.TemporaryFile('w+b')
+    f.write(b'Testing')
+    f.seek(0)
+
+
+def test_storing_file_locally(infile):
     """
     Writes content from one file-like to another
     """
-    infile = tempfile.TemporaryFile('w+b')
-    infile.write(b'Testing')
-    infile.seek(0)
-
     outfile = tempfile.NamedTemporaryFile(delete=False)
     storage.local(infile,outfile)
-
     with open(outfile.name,'rb') as f:
         assert f.read() == b'Testing'
+
+def test_storing_file_os_aws_s3(mocker,infile):
+    """
+    Writes content from one file-line to S3
+    """
+    client = mocker.Mock()
+
+    storage.s3(client, infile, "bucket", "file-name")
+
+    client.ipload_fileobj.assert_called_with(infile, "bucket", "file-name")
